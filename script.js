6281be561acfd1b7fdfa175410fe2cf2a750e2a8
William Rodrigues <williamalkamim07@gmail.com>
2025-12-28 22:49:51 -0300

    Update script.js
    Added WhatsApp numbers to locais and updated popups.

--- script.js
+++ script.js
@@ -45,56 +45,67 @@ const locais = [
     {
         nome: "Eunice de Almeida Santos",
         endereco: "Rua Luiz Antonio Gonçalves, 401",
+        numero: "11 96552-6714",
         coords: [-23.535960377280276, -46.46032369144317]
     },
     {
         nome: "Feliciano e Maria Dória",
         endereco: "Rua Luiz Antonio Gonçalves, 419",
+        numero: "11 94853-8611",
         coords: [-23.536038201498194, -46.46020428480048]
     },
     {
         nome: "José Lúcio e Josefa",
         endereco: "Rua Arte do Sol, 19 D",
+        numero: "11 96217-5991",
         coords: [-23.52788578836052, -46.460886718004815] 
     },
     {
         nome: "Ester Loula Santos",
         endereco: "Rua Monte Car, 1563",
+        numero: "11 96319-4946",
         coords: [-23.52324818290377, -46.460652863851216]
     },
     {
         nome: "Cleonice Maximiliano",
         endereco: "Rua Arreio de Prata, 23",
+        numero: "11 99398-4652",
         coords: [-23.525007894527647, -46.460159395906]
     },
     {
         nome: "Marilene Messias de Souza",
         endereco: "Rua José Vieira Guimarães, 243",
+        numero: "11 99817-6896",
         coords: [-23.52310530248178, -46.428089487011675]
     },
     {
         nome: "Eunice Maria de Souza",
         endereco: "Rua Cananga do Japão, 20",
+        numero: "11 97018-3780",
         coords: [-23.53859701152427, -46.459731142243946]
     },
     {
         nome: "Geralda José da Silva",
         endereco: "Rua José Alves dos Santos, 128",
+        numero: "11 98104-2630",
         coords: [-23.53861226691447, -46.461466940573494]
     },
     {
         nome: "Maria Aurora",
         endereco: "Rua José Alves dos Santos, 129",
+        numero: "11 98491-9075",
         coords: [-23.53860083497892, -46.46169813650688]
     },
     {
         nome: "Maria do Carmo",
         endereco: "Rua Paranátama, 175",
+        numero: "11 93210-9008",
         coords: [-23.545151759033814, -46.45524142809184]
     },
     {
         nome: "Marise",
         endereco: "Rua Inula, 7",
+        numero: "11 98997-0881",
         coords: [-23.513270717668977, -46.44630732724521]
     }
 ];
@@ -107,6 +118,7 @@ locais.forEach(function(local) {
         var conteudoPopup = `
             <div class="popup-content">
                 <h3>${local.nome}</h3>
+                <p><strong>WhatsApp:</strong> ${local.numero}</p>
                 <p><strong>Endereço:</strong> ${local.endereco}</p>
                 <a href="https://www.google.com/maps/dir/?api=1&destination=${encodeURIComponent(local.endereco)}" target="_blank" style="color:blue; text-decoration:underline;">
                    Traçar Rota até aqui
